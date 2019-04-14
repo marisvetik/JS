51fb406ec799f788dff1650c9ef7264e38b35051
marisvetik <marisvetik@gmail.com>
2019-04-14 23:17:59 +0300

    Task1 done

--- Homework/Module01/index.js
+++ Homework/Module01/index.js
@@ -1,1 +1,17 @@
 'use strict'
+
+const login = prompt('Пожалуйста введите пароль');
+const ADMIN_PASSWORD = 'm4ng0h4ckz';
+let message;
+
+if (login !== null) {
+    if (login === ADMIN_PASSWORD) {
+        alert('Добро пожаловать!');
+    } else {
+        alert('Доступ запрещен, неверный пароль!');
+    }
+} else {
+    alert('Отменено пользователем!');
+}
+
+console.log(message(alert));
