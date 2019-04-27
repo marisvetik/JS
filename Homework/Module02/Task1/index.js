521604f867378776ea9689685e99dce7209e1fd4
marisvetik <marisvetik@gmail.com>
2019-04-27 18:00:48 +0300

    task01 fixed

--- Homework/Module02/Task1/index.js
+++ Homework/Module02/Task1/index.js
@@ -6,16 +6,16 @@ let total = 0;
 
 do {
     input = prompt('Пожалуйста введите число');
-    if(isNaN(Number(input))) {
+    if(Number.isNaN(Number(input))) {
         alert('Было введено не число, попробуйте еще раз');
     } else if(input !== null) {
         numbers.push(Number(input));
     }
     } while (input !== null);
 
-if(numbers.length !== 0) {
+if(numbers.length) {
     for(let element of numbers) {
-        total += Number(element);
+        total += element;
     }
     console.log(`Общая сумма чисел равна ${total}`);
 }
